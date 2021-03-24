fd6b2906bf13b48ff5c564d4d3015c5c46054dd7
WheelKillu <1342882983@qq.com>
2021-03-24 14:05:59 +0800

    deploy

---
+++ precache-manifest.3ed9a33c2d61847cc442202bcd849b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b417b2a0dac09dce81b",
+    "url": "/keep-account-website/css/app.cd34bfcc.css"
+  },
+  {
+    "revision": "1856484988479eadb6780208872582eb",
+    "url": "/keep-account-website/index.html"
+  },
+  {
+    "revision": "0b417b2a0dac09dce81b",
+    "url": "/keep-account-website/js/app.dc57652b.js"
+  },
+  {
+    "revision": "8454b36a4f862780071f",
+    "url": "/keep-account-website/js/chunk-vendors.d6e6c561.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/keep-account-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keep-account-website/robots.txt"
+  }
+]);
